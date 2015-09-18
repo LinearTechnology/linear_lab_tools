44e8f3047263a7055744d5b36be4cfd4394e2341
jsorensen <jsorensen@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2015-09-18 23:48:31 +0000

    added fpga load function
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@4012 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/ltc_controller_comm.py
+++ python/ltc_controller_comm.py
@@ -747,6 +747,17 @@ class Controller(object):
             self.dll.LccFpgaGetIsLoaded(self._handle, ct.c_char_p(fpga_filename),
                                         ct.byref(is_loaded)))
         return is_loaded.value
+        
+    def fpga_load_file(self, fpga_filename):
+        """
+        Loads an FPGA file
+        
+        :param fpga_filename: The base file name without any folder, extension
+            or revision info, for instance 'DLVDS' or 'S2175', case insensitive.
+            :return: True if the requested load is loaded, False otherwise.
+        """
+        self._raise_on_error(
+            self.dll.LccFpgaLoadFile(self._handle, ct.c_char_p(fpga_filename))
 
     def eeprom_read_string(self, num_chars):
         """Receive an EEPROM string over bit-banged I2C via FPGA register."""
