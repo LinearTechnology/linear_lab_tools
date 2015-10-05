f3e396fb86a2747845c48319e283b1ded46749eb
jsorensen <jsorensen@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2015-10-05 22:00:29 +0000

    added 2261 (890) and 2268 (1371)
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@4146 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/full_tests/all_tests.py
+++ python/full_tests/all_tests.py
@@ -1,6 +1,8 @@
 import test_ltc2000 as t1
 import test_ltc2123 as t2
 import test_mini_module as t3
+import test_ltc2261 as t4
+import test_ltc2268 as t5
 
 print 'High Speed tests'
 
@@ -11,8 +13,10 @@ t2.test()
 print 'Test battery 3'
 t3.test()
 
-print 'DC718 tests'
-
 print 'DC890 tests'
+print 'Test battery 4'
+t4.test()
 
 print 'DC1371 tests'
+print 'Test battery 5'
+t5.test()
