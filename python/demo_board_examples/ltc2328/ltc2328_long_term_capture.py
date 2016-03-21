71f41737c09d0f7b849ce6d15c78f3e71641f449
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-03-21 19:11:07 +0000

    Minor edits... (switching machines...)
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@4796 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/demo_board_examples/ltc2328/ltc2328_long_term_capture.py
+++ python/demo_board_examples/ltc2328/ltc2328_long_term_capture.py
@@ -36,20 +36,24 @@ either expressed or implied, of Linear Technology Corp.
 '''
 
 from time import sleep
+import time
 import numpy as np
 
 from ltc2328_dc1908 import ltc2328_dc1908
 
 # Print extra information to console
 
-num_points = 128
+num_points = 1024
 delay = 1.0
 
 averages = [0] * num_points
 stdevs = [0] * num_points
+times = [time.time()] * num_points
+starttime = time.time()
 
 for i in range (0, num_points):
     print("Capturing point " + str(i) + " of " + str(num_points))
+    times[i] = time.time() - starttime
     capturedata = ltc2328_dc1908(32*1024, verbose=False, do_demo=False)
     averages[i] = np.average(capturedata)
     stdevs[i] = np.std(capturedata)
@@ -62,4 +66,4 @@ for i in range (0, num_points):
 print('Writing data to file')
 with open('longterm_data.txt', 'w') as f:
     for i in range (0, num_points):
-        f.write(str(averages[i]) + "," + str(stdevs[i]) + '\n')
+        f.write(str(times[i]) + "," + str(averages[i]) + "," + str(stdevs[i]) + '\n')
