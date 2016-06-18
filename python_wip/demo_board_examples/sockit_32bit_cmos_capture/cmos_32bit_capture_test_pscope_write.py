626eb7f88b254c52ac93b26d6df4ab18d0964fe9
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-06-18 15:12:10 +0000

    Commit before delete - this is deprecated, replaced by DC2512 in python/demo_board_examples.
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@5305 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python_wip/demo_board_examples/sockit_32bit_cmos_capture/cmos_32bit_capture_test_pscope_write.py
+++ python_wip/demo_board_examples/sockit_32bit_cmos_capture/cmos_32bit_capture_test_pscope_write.py
@@ -12,36 +12,20 @@
 import sys #, os, socket, ctypes, struct
 sys.path.append("../../")
 sys.path.append("../../../python/utils/")
-sys.path.append("../../../python/app_examples\ltc2500_family/")
+sys.path.append("../../../python/app_examples/ltc2500_family/")
 from save_for_pscope import save_for_pscope
 import numpy as np
 #from subprocess import call
 from time import sleep
 from matplotlib import pyplot as plt
 # Okay, now the big one... this is the module that communicates with the SoCkit
-from mem_func_client import MemClient
+from mem_func_client_2 import MemClient
 from DC2390_functions import *
 
 # Get the host from the command line argument. Can be numeric or hostname.
 #HOST = sys.argv.pop() if len(sys.argv) == 2 else '127.0.0.1'
 HOST = sys.argv[1] if len(sys.argv) == 2 else '127.0.0.1'
 
-
-# Parameters for PID example
-PID_KP = 0x0010
-PID_KI = 0x0040
-PID_KD = 0x0050
-
-PULSE_LOW = 20000
-PULSE_HIGH = 100000
-PULSE_VAL = 25000
-
-FOS_TAU = 0x8000
-FOS_GAIN = 0x0002
-FOS_CLOCK = 4
-
-SYSTEM_CLOCK_DIVIDER = 199
-LUT_NCO_DIVIDER = 0xFFFF # 0xFFFF for divide by 1
 NUM_SAMPLES = 2**20#65536 #131072 #8192
 
 DEADBEEF = -559038737 # For now, need to re-justify.
@@ -49,17 +33,6 @@ DEADBEEF = -559038737 # For now, need to re-justify.
 CONTROL_LOOP = 0x02
 ADC_B_CAPTURE = 0x00
 CHANNEL = ADC_B_CAPTURE
-
-N = 7 #Number of samples to average (LTC2380-24)
-
-nco_word_width = 32
-master_clock = 50000000
-bin_number = 23 # Number of cycles over the time record
-sample_rate = master_clock / (SYSTEM_CLOCK_DIVIDER + 1) # 250ksps for 50M clock, 200 clocks per sample
-cycles_per_sample = float(bin_number) / float(NUM_SAMPLES)
-cycles_per_dac_sample = cycles_per_sample / (SYSTEM_CLOCK_DIVIDER + 1)
-tuning_word = int(cycles_per_dac_sample * 2**nco_word_width)
-print("Tuning Word:" + str(tuning_word))
 
 
 print('Starting client')
@@ -74,16 +47,7 @@ print ('FPGA load revision: %04X' % rev)
 
 #datapath fields: lut_addr_select, dac_a_select, dac_b_select[1:0], fifo_data_select
 #lut addresses: 0=lut_addr_counter, 1=dac_a_data_signed, 2=0x4000, 3=0xC000
-# DAC A: 
-#	.data0x ( nco_sin_out ),
-#	.data1x ( pid_output ),
-#	.data2x ( 16'h4000 ),
-#	.data3x ( 16'hC000 ),
-# DAC B:
-#	.data0x ( nco_cos_out ),
-#	.data1x ( lut_output ),
-#	.data2x ( 16'hC000 ),
-#	.data3x ( 16'h4000 ),
+
 # FIFO Data:
 # 0 = ADC A
 # 1 = ADC B
@@ -104,9 +68,9 @@ print ('Okay, now lets blink some lights and run some tests!!')
 print ('First test - sweep a couple of sinewaves on ADC B')
 
 # print ('run # %d ' % i)
-client.reg_write(LED_BASE, (N << 16) | 0x0F) #Was 234
+client.reg_write(LED_BASE, 0x01)
 sleep(0.1)
-client.reg_write(LED_BASE, (N << 16) | 0x00) #Was 234
+client.reg_write(LED_BASE, 0x00)
 sleep(0.1)
 
 
@@ -115,10 +79,19 @@ client.reg_write(DATAPATH_CONTROL_BASE, DC2390_FIFO_ADCA_NYQ) # First capture AD
 data = capture(client, NUM_SAMPLES, trigger = 0, timeout = 2.0)
 
 data_ch0 = np.ndarray(NUM_SAMPLES, dtype=float)
-for i in range(0, NUM_SAMPLES):
-    data_ch0[i] = (data[i] & 0x0003FFFF)
-    if(data_ch0[i] > 2**17):
-        data_ch0[i] -= 2**18
+
+numbits = 16
+if(numbits == 18):
+    for i in range(0, NUM_SAMPLES):
+        data_ch0[i] = (data[i] & 0x0003FFFF)
+        if(data_ch0[i] > 2**17):
+            data_ch0[i] -= 2**18
+
+if(numbits == 16):
+    for i in range(0, NUM_SAMPLES):
+        data_ch0[i] = (data[i] & 0x0000FFFF)
+        if(data_ch0[i] > 2**15):
+            data_ch0[i] -= 2**16
 
 data_nodc0 = data_ch0 #- np.average(data)
 
@@ -136,15 +109,7 @@ save_for_pscope("pscope_DC2390.adc",24 ,True, NUM_SAMPLES, "2390", "2500",
 
 
 client.reg_write(DATAPATH_CONTROL_BASE, DC2390_FIFO_UP_DOWN_COUNT) # Capture a test pattern
-data = capture(client, NUM_SAMPLES, trigger = 0, timeout = 2.0)
 
-
-#data_nodc *= np.blackman(NUM_SAMPLES)
-#fftdata = np.abs(np.fft.fft(data_nodc)) / NUM_SAMPLES
-#fftdb = 20*np.log10(fftdata / 2.0**31)
-plt.figure(pltnum)
-plt.axis([0, NUM_SAMPLES, -1*(2**31), 2**31])
-plt.plot(data)
-print data[0:10]
-
-
+print("Ramp test!")
+errors = ramp_test(client, 2**21, trigger = 0, timeout = 1.0)
+print("Number of errors: " + str(errors))
