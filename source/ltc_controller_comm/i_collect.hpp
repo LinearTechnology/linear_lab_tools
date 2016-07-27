6fe7c3f443bac1003fd8caf4d4f9a2ba0af86763
jsorensen <jsorensen@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-07-27 16:53:29 +0000

    can't see any change
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@5509 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- source/ltc_controller_comm/i_collect.hpp
+++ source/ltc_controller_comm/i_collect.hpp
@@ -1,18 +1,18 @@
 #pragma once
 #include "controller.hpp"
 namespace linear {
-    class ICollect : virtual public Controller {
-    public:
-        enum class Trigger {
-            NONE = LCC_TRIGGER_NONE,
-            START_POSITIVE_EDGE = LCC_TRIGGER_START_POSITIVE_EDGE,
-            DC890_START_NEGATIVE_EDGE = LCC_TRIGGER_DC890_START_NEGATIVE_EDGE,
-            DC1371_STOP_NEGATIVE_EDGE = LCC_TRIGGER_DC1371_STOP_NEGATIVE_EDGE,
-        };
-        virtual void DataStartCollect(int total_samples, Trigger trigger) = 0;
-        virtual bool DataIsCollectDone() = 0;
-        virtual void DataCancelCollect() = 0;
-        virtual ~ICollect() { }
+class ICollect : virtual public Controller {
+public:
+    enum class Trigger {
+        NONE = LCC_TRIGGER_NONE,
+        START_POSITIVE_EDGE = LCC_TRIGGER_START_POSITIVE_EDGE,
+        DC890_START_NEGATIVE_EDGE = LCC_TRIGGER_DC890_START_NEGATIVE_EDGE,
+        DC1371_STOP_NEGATIVE_EDGE = LCC_TRIGGER_DC1371_STOP_NEGATIVE_EDGE,
     };
+    virtual void DataStartCollect(int total_samples, Trigger trigger) = 0;
+    virtual bool DataIsCollectDone() = 0;
+    virtual void DataCancelCollect() = 0;
+    virtual ~ICollect() { }
+};
 }
 
