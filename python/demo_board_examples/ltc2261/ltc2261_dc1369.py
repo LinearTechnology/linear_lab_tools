5085f58778c7ddb5d2713cfe2dd17e9dac09722d
nquintero <nquintero@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-09-12 22:42:23 +0000

    new version
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@5703 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/demo_board_examples/ltc2261/ltc2261_dc1369.py
+++ python/demo_board_examples/ltc2261/ltc2261_dc1369.py
@@ -53,143 +53,46 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of Linear Technology Corp.
 '''
 
-from time import sleep
-import numpy as np
-# Import communication library
-import sys
-sys.path.append("../../")
-import ltc_controller_comm as comm
+import llt.common.dc890 as dc890
+import llt.common.functions as funcs
+import llt.common.constants as consts
 
-def ltc2261_dc1369(num_samples, verbose=False, do_demo=False):
-    if do_demo:
-        plot_data = True
-        write_to_file = True
-    else:
-        plot_data = False
-        write_to_file = False
+def ltc2261_dc1369(num_samples, spi_registers, verbose = False, do_plot = False, 
+                      do_write_to_file = False):
+    with Dc1369(spi_registers, verbose) as controller:
+        # You can call this multiple times with the same controller if you need to
+        data = controller.collect(num_samples, consts.TRIGGER_NONE)
+        data = data[0::2] # Take every even sample
+        if do_plot:
+            funcs.plot(data)
+        if do_write_to_file:
+            funcs.write_to_file_32_bit(data, "data.txt")
+        return data
 
-    # set TEST_DATA_REG to one of these constants
-    DATA_REAL = 0x00
-    DATA_ALL_ZEROS = 0x08
-    DATA_ALL_ONES = 0x18
-    DATA_CHECKERBOARD = 0x28
-    DATA_ALTERNATING = 0x38
-
-    TEST_DATA_REG = DATA_REAL
-
-    SAMPLE_BYTES = 2
-    
-    num_samples_x2 = num_samples * 2
-    
-    def vprint(s):
-        """Print string only if verbose is on"""
-        if verbose:
-            print s
-
-   # find demo board with correct ID
-    EXPECTED_EEPROM_ID = 'LTC2261-14,D9002,DC1369A-A,YEE232T,DLVDS,-------'
-    device_info = None
-    vprint('Looking for a DC890 with a DC1369A-A demoboard')
-    for info in comm.list_controllers(comm.TYPE_DC890):
-        with comm.Controller(info) as device:
-            eeprom_id = device.eeprom_read_string(len(EXPECTED_EEPROM_ID))
-            if 'DC1369' in eeprom_id:
-                if verbose:
-                    print 'Found a DC1369 demoboard'
-                device_info = info
-                break
-    vprint(device_info)
-    if device_info is None:
-        raise(comm.HardwareError('Could not find a compatible device'))
-    # Open communication to the demo board
-    with comm.Controller(device_info) as controller:
-
-        controller.dc890_gpio_set_byte(0xF8)
-        controller.dc890_gpio_spi_set_bits(3, 0, 1)
-
-        vprint('Configuring SPI registers')
-        if TEST_DATA_REG == DATA_REAL:
-            vprint('Set to read real data')
-        else:
-            vprint('Set to generate test data')
-            
-        controller.spi_send_byte_at_address(0x00, 0x80)
-        controller.spi_send_byte_at_address(0x01, 0x00)
-        # Set address 0x02 to a value of 0x01 to enable DCS (useful if clock is not 50% Duty Cycle)
-        controller.spi_send_byte_at_address(0x02, 0x00)
-        controller.spi_send_byte_at_address(0x03, 0x71)
-        controller.spi_send_byte_at_address(0x04, TEST_DATA_REG)
-
-        if not controller.fpga_get_is_loaded("DLVDS"):
-            vprint('Loading FPGA')
-            controller.fpga_load_file("DLVDS")
-        else:
-            vprint('FPGA already loaded')
-
-        vprint('Starting data collect')
-        # Due to a quirk of the DC1369 and the DDR LVDS load, we have to claim this
-        # is a 2 channel part and then throw away the data from the second channel
-        # this means we only get half of what we ask for (so we ask for twice as much).
-        controller.data_set_high_byte_first()
-        controller.data_set_characteristics(True, SAMPLE_BYTES, True)
-        controller.data_start_collect(num_samples_x2, comm.TRIGGER_NONE)
-        for i in range(10):
-            is_done = controller.data_is_collect_done()
-            if is_done:
-                break
-            sleep(0.2)
-
-        if not is_done:
-            raise comm.HardwareError('Data collect timed out (missing clock?)')
-        vprint('Data collect done.')
-
-        controller.dc890_flush()
-
-        vprint('Reading data')
-        num_bytes, data = controller.data_receive_uint16_values(end=num_samples_x2)
-        vprint('Data read done')
-
-        # keep data from first channel only
-        data_ch1 = [data[2*i] & 0x3FFF for i in range(num_samples)]
-
-        vprint('Writing data to file "data.txt"')
-        with open('data.txt', 'w') as f:
-            for item in data_ch1:
-                f.write(str(item & 0x3FFF) + '\n')
-        vprint('File write done.')
-
-        if plot_data:
-            from matplotlib import pyplot as plt
-            plt.figure(1)
-            plt.plot(data_ch1)
-            plt.title('Time Domain Data')
-
-            plt.show()
-
-            ADC_AMPLITUDE = 16384.0 / 2.0
-
-            windowscale = (num_samples) / sum(np.blackman(num_samples))
-            vprint("Window scaling factor: " + str(windowscale))
-
-            data_ch1 -= np.average(data_ch1)
-            windowed_data_ch1 = data_ch1 * np.blackman(num_samples) * windowscale # Apply Blackman window
-            freq_domain_ch1 = np.fft.fft(windowed_data_ch1)/(num_samples) # FFT
-            freq_domain_magnitude_ch1 = np.abs(freq_domain_ch1) # Extract magnitude
-            freq_domain_magnitude_db_ch1 = 20 * np.log10(freq_domain_magnitude_ch1/ADC_AMPLITUDE)
-
-            plt.figure(2)
-            plt.title('Frequency Domain')
-            plt.plot(freq_domain_magnitude_db_ch1)
-
-            plt.show()
-
-        vprint('All finished!')
-    
-        return data_ch1
+class Dc1369(dc890.Demoboard):
+    """
+        A DC890 demo board with settings for the DC1369
+    """
+    def __init__(self, spi_registers, verbose = False):
+        dc890.Demoboard.__init__(self, 
+                                 dc_number             = 'DC1369', 
+                                 fpga_load             = 'DLVDS',
+                                 num_channels          = 2,
+                                 is_positive_clock     = True, 
+                                 num_bits              = 14,
+                                 alignment             = 14,
+                                 is_bipolar            = False,
+                                 spi_reg_values        = spi_registers,
+                                 verbose               = verbose)
 
 if __name__ == '__main__':
     NUM_SAMPLES = 64 * 1024
+    spi_reg = [0x00, 0x80,
+               0x01, 0x00,
+               0x02, 0x00,
+               0x03, 0x71,
+               0x04, 0x00]
     # to use this function in your own code you would typically do
-    # data = ltc2261_dc1369(num_samples)
-    ltc2261_dc1369(NUM_SAMPLES, verbose=True, do_demo=True)
+    # data = ltc2261_dc1369(num_samples, spi_reg)
+    ltc2261_dc1369(NUM_SAMPLES, spi_reg, verbose=True, do_plot=True)
 
