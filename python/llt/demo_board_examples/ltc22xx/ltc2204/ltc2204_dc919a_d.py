1ff8092d92ff5dc3c427947037c945b7e3a32635
nquintero <nquintero@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-10-18 18:03:58 +0000

    added new demo board
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@5945 2d5ccaef-af5a-4037-8d26-727f02bf293a

---
+++ python/llt/demo_board_examples/ltc22xx/ltc2204/ltc2204_dc919a_d.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+    E-mail: quikeval@linear.com
+
+    Copyright (c) 2016, Linear Technology Corp.(LTC)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, 
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright 
+       notice, this list of conditions and the following disclaimer in the 
+       documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+    POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are 
+    those of the authors and should not be interpreted as representing official
+    policies, either expressed or implied, of Linear Technology Corp.
+
+    Description:
+        The purpose of this module is to demonstrate how to communicate with 
+        the LTC2204 demo board through python with the DC718.
+"""
+
+import llt.common.dc718 as dc718
+import llt.common.functions as funcs
+import llt.common.constants as consts
+
+def ltc2204_dc919a_d(num_samples, verbose = False, do_plot = False, 
+                      do_write_to_file = False):
+    # connect to the DC919A-D and do a collection
+    with Dc919aD(verbose) as controller:
+        # You can call this multiple times with the same controller if you need to
+        data = controller.collect(num_samples, consts.TRIGGER_NONE)
+        
+        if do_plot:
+            funcs.plot(data, controller.get_num_bits(), verbose=verbose)
+        if do_write_to_file:
+            funcs.write_to_file_32_bit("data.txt", data, verbose=verbose)
+        return data
+
+class Dc919aD(dc718.Demoboard):
+    """
+        A DC718 demo board with settings for the DC919A-D
+    """
+    def __init__(self, verbose = False):
+        dc718.Demoboard.__init__(self, 
+                                 dc_number             = 'DC919A-D', 
+                                 is_positive_clock     = True, 
+                                 num_bits              = 16, 
+                                 alignment             = 16,
+                                 is_bipolar            = True,                                      
+                                 verbose               = verbose)
+
+if __name__ == '__main__':
+    NUM_SAMPLES = 32 * 1024
+    # to use this function in your own code you would typically do
+    # data = ltc2204_dc919a_d(num_samples)
+    testdata = ltc2204_dc919a_d(NUM_SAMPLES, verbose=True, do_plot = True, 
+                             do_write_to_file = True)
