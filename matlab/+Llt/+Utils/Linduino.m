aae7ef39e58e600be4a95b1220bae5d27e1fe64a
jsorensen <jsorensen@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-10-28 16:28:49 +0000

    fixed the call to clear, added some status prints and made the serial timeout go away and got rid of a lint.
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@5974 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- matlab/+Llt/+Utils/Linduino.m
+++ matlab/+Llt/+Utils/Linduino.m
@@ -1,10 +1,11 @@
 function ser = Linduino()
 ser = [];
+warning('off', 'MATLAB:serial:fgetl:unsuccessfulRead');
 for port = 0:255
-    s = serial(sprintf('COM%d', port), 'BaudRate', 115200);
+    s = serial(sprintf('COM%d', port), 'BaudRate', 115200, 'Timeout', 1); %#ok
     try
         fopen(s);
-    catch
+    catch %#ok if there is an error just try the next one
         delete(s);
         clear s;
         continue;
@@ -15,11 +16,15 @@ for port = 0:255
     idStr = fgetl(s);
     if strcmp(idStr(21:25), 'DC590')
         ser = s;
+        fprintf('Port %d appears to be a DC590 or Linduino\n', port);
+        warning('on', 'MATLAB:serial:fgetl:unsuccessfulRead');
         return;
     else 
         fclose(s);
         delete(s);
-        clear(s);
+        clear s;
     end
 end
+fprintf('Did not find a DC590 or Linduino');
+warning('on', 'MATLAB:serial:fgetl:unsuccessfulRead');
         
