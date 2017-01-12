f26a4a99992652fca2827981deeccd9b64aad8cb
jsorensen <jsorensen@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2017-01-12 17:41:36 +0000

    adding folders for ltm90xx boards
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@6321 2d5ccaef-af5a-4037-8d26-727f02bf293a

---
+++ python/llt/demo_board_examples/ltm90xx/__init__.py
@@ -0,0 +1,8 @@
+print "importing package 'demo_board_examples'"
+# from dc2512 import *
+# from ltc16xx import *
+# from ltc21xx import *
+from ltc22xx import ltc2261
+# from ltc23xx import *
+# from ltc2000 import *
+# from ltm9011 import *
