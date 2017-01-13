287b4b60fe06ed65f32a455982673560cf9970c0
nquintero <nquintero@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2017-01-13 17:47:25 +0000

    Fixed these demo boards by adding a foo file to point to the correct file
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@6358 2d5ccaef-af5a-4037-8d26-727f02bf293a

---
+++ matlab/+llt/+demo_board_examples/+ltc23xx/+ltc2382/ltc2382_dc1571b_b.m
@@ -0,0 +1,37 @@
+% DC1571B-B / LTC2382 Interface Example
+%
+% The purpose of this function is to point the user to the correct function.
+% 
+% REVISION HISTORY
+% $Revision: 5756 $
+% $Date: 2016-09-16 12:58:34 -0700 (Fri, 16 Sep 2016) $
+%
+% Copyright (c) 2017, Linear Technology Corp.(LTC)
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, 
+%    this list of conditions and the following disclaimer.
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+% ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+% ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+% ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+% 
+% The views and conclusions contained in the software and documentation are those
+% of the authors and should not be interpreted as representing official policies,
+% either expressed or implied, of Linear Technology Corp.
+
+function varargout = ltc2382_dc1571b_b(varargin)
+    error('LtcControllerComm:NotSupportedError', 'Please use ltc2382_dc1571a_b.m to run this demo board');
