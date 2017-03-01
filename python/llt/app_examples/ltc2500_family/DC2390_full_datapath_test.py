27ff5b452fa77fdea848ade4f2b1718d25fa1a14
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2017-03-01 00:55:55 +0000

    Long overdue cleanup.
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@6604 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/llt/app_examples/ltc2500_family/DC2390_full_datapath_test.py
+++ python/llt/app_examples/ltc2500_family/DC2390_full_datapath_test.py
@@ -1,35 +1,69 @@
 #!/usr/bin/python
-# Example application for running tests on the Arrow SoCkit board
-# using the LT_soc_framework
-
-# Big update 6/29/2015!!
-# Switching to true ring buffer operation using Noe's FPGA code
-
-# 7/23/2015 - All data paths in FPGA design are in place!!
-# Splitting out a bunch of stuff into DC2390_functions.py module
+# -*- coding: utf-8 -*-
+'''
+Example application for running tests on the Arrow SoCkit board
+using the LT_soc_framework
 
 
-import sys #, os, socket, ctypes, struct
+Tested with Python 2.7, Anaconda distribution available from Continuum Analytics,
+http://www.continuum.io/
 
-from llt.utils.save_for_pscope import save_for_pscope
+
+REVISION HISTORY
+$Revision: 6460 $
+$Date: 2017-01-30 14:26:33 -0800 (Mon, 30 Jan 2017) $
+
+Copyright (c) 2015, Linear Technology Corp.(LTC)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of Linear Technology Corp.
+
+'''
+
+import sys , ctypes
 import numpy as np
-#from subprocess import call
 from time import sleep
 from matplotlib import pyplot as plt
-# Okay, now the big one... this is the module that communicates with the SoCkit
-# sys.path.append('C:\Users\MSajikumar\Documents\LT_soc_framework')
+
+# Save data in PScope compatible format
+from llt.utils.save_for_pscope import save_for_pscope
+# this is the module that communicates with the SoCkit
 from llt.common.mem_func_client_2 import MemClient
-from DC2390_functions import *
+# Functions and defines specific to this board
+from llt.utils.DC2390_functions import *
+# Functions and defines specific to sockit system
 from llt.utils.sockit_system_functions import *
 
 # Get the host from the command line argument. Can be numeric or hostname.
-#HOST = sys.argv.pop() if len(sys.argv) == 2 else '127.0.0.1'
-HOST = sys.argv[1] if len(sys.argv) == 2 else '127.0.0.1'
+HOST = sys.argv[1] if len(sys.argv) >= 2 else '127.0.0.1'
 
-
+# Enable / disable simple memory bandwidth test using internal digital
+# pattern generator
 mem_bw_test = False
 
-# Parameters for PID example
+# Initial Parameters for PID example
 PID_KP = 0x0010
 PID_KI = 0x0040
 PID_KD = 0x0050
@@ -38,44 +72,35 @@ PULSE_LOW = 20000
 PULSE_HIGH = 100000
 PULSE_VAL = 25000
 
-FOS_TAU = 0x8000
-FOS_GAIN = 0x0002
-FOS_CLOCK = 4
+# Set to true to write a downward ramp to DAC lookup table
+test_LUT_write = True
 
-SYSTEM_CLOCK_DIVIDER = 199
+# Timing / data record length
+SYSTEM_CLOCK_DIVIDER = 199 # 50MHz / (n+1), set to 99 for 500ksps, 199 for 250ksps, etc.
 LUT_NCO_DIVIDER = 0xFFFD # 0xFFFF for divide by 1
 NUM_SAMPLES = 8192 #131072 #8192
 
-DEADBEEF = -559038737 # For now, need to re-justify.
-
-CONTROL_LOOP = 0x02
-ADC_B_CAPTURE = 0x00
-CHANNEL = ADC_B_CAPTURE
+DEADBEEF = -559038737 # For checking againtst constant test pattern
 
 N = 7 #Number of samples to average (LTC2380-24)
 
 vfs = 10.0 # Full-scale voltage, VREF * 2 for LTC25xx family
 
-nco_word_width = 32
-master_clock = 50000000
-bin_number = 50 # Number of cycles over the time record
+nco_word_width = 32 # A property of the NCO in the FPGA design
+master_clock = 50000000 # Relies on proper configuration of LTC6954 divider
+bin_number = 50 # Number of cycles over the time record for sine signal
 sample_rate = master_clock / (SYSTEM_CLOCK_DIVIDER + 1) # 250ksps for 50M clock, 200 clocks per sample
 cycles_per_sample = float(bin_number) / float(NUM_SAMPLES)
 cycles_per_dac_sample = cycles_per_sample / (SYSTEM_CLOCK_DIVIDER + 1)
 tuning_word = int(cycles_per_dac_sample * 2**nco_word_width)
 print("Tuning Word:" + str(tuning_word))
 
-
 print('Starting client')
 client = MemClient(host=HOST)
 #First thing's First!! Configure clocks...
 LTC6954_configure(client, 0x04)
 #Read FPGA type and revision
-rev_id = client.reg_read(REV_ID_BASE)
-type_id = rev_id & 0x0000FFFF
-rev = (rev_id >> 16) & 0x0000FFFF
-print ('FPGA load type ID: %04X' % type_id)
-print ('FPGA load revision: %04X' % rev)
+type_rev_check(client, 0xABCD, 0x1246)
 
 print("Setting up system parameters.\n");
 client.reg_write(SYSTEM_CLOCK_BASE, SYSTEM_CLOCK_DIVIDER)
@@ -118,26 +143,23 @@ pltnum = 1
 # std_ctrl_wire = {26'bz, lut_write_enable, ltc6954_sync , gpo1, gpo0, en_trig, start };
 
 print ('Okay, now lets blink some lights and run some tests!!')
-print ('First test - sweep a couple of sinewaves on ADC B')
 
 # print ('run # %d ' % i)
-client.reg_write(LED_BASE, (N << 16) | 0x0F) #Was 234
+client.reg_write(LED_BASE, (N << 16) | 0x0F) # LED_BASE register also contains 
 sleep(0.1)
-client.reg_write(LED_BASE, (N << 16) | 0x00) #Was 234
+client.reg_write(LED_BASE, (N << 16) | 0x00) # LTC2500 filter configuration.
 sleep(0.1)
 
-client.reg_write(TUNING_WORD_BASE, tuning_word) # Sweep NCO!!!
+client.reg_write(TUNING_WORD_BASE, tuning_word) # Set NCO frequency
 
-NUM_SAMPLES_SAV = NUM_SAMPLES
-NUM_SAMPLES = NUM_SAMPLES#2**21
 # Capture a sine wave
-client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_sines) # Sweep NCO!!!
-data = sockit_ltc2500_to_signed32(sockit_capture(client, NUM_SAMPLES, trigger = TRIG_NOW, timeout = 0.0))
-#data = sockit_ltc2500_to_signed32(sockit_capture(client, 2**21, trigger = TRIG_NOW, timeout = 0.0))
-rms = np.std(data)
+client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_sines) # Set data multiplexers
+# Capture data - LTC2500 Nyquist data first bit is overrange, followed by MSB
+data = sockit_ltc2500_to_signed32(sockit_capture(client, NUM_SAMPLES, trigger = TRIG_NOW, timeout = 1.0))
+rms = np.std(data) # Useful for checking noise levels, signal amplitudes.
 print("Standard Deviation: " + str(rms))
-data_nodc = data - np.average(data)
-data_nodc *= np.blackman(NUM_SAMPLES)
+data_nodc = data - np.average(data)       # Strip out DC content, required before windowing
+data_nodc *= np.blackman(NUM_SAMPLES)     # Apply Blackman window
 fftdata = np.abs(np.fft.fft(data_nodc)) / NUM_SAMPLES
 fftdb = 20*np.log10(fftdata / 2.0**31)
 plt.figure(pltnum)
@@ -148,7 +170,6 @@ plt.plot(data)
 plt.subplot(2, 1, 2)
 plt.plot(fftdb)
 
-NUM_SAMPLES = NUM_SAMPLES_SAV
 
 
 data_ndarray = np.array(data)
@@ -161,7 +182,7 @@ print("Peak-to-Peak voltage: " + str(datap_p))
 data_for_pscope = data_nodc / 256.0
 save_for_pscope("pscope_DC2390.adc",24 ,True, NUM_SAMPLES, "2390", "2500", data_for_pscope)
 
-# Run through lookup table continuously
+# Run through lookup table continuously (Arbitrary waveform generator mode)
 client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_lut_continuous)
 data = sockit_ltc2500_to_signed32(sockit_capture(client, NUM_SAMPLES, trigger = TRIG_NOW, timeout = 0.0))
 fftdata = np.abs(np.fft.fft(data)) / len(data)
@@ -174,7 +195,7 @@ plt.plot(data)
 plt.subplot(2, 1, 2)
 plt.plot(fftdb)
 
-# Run through lookup table once (pulse test)
+# Run through lookup table once (Arbitrary pulse mode)
 client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_lut_run_once)
 data = sockit_ltc2500_to_signed32(sockit_capture(client, NUM_SAMPLES, trigger = TRIG_NOW, timeout = 0.0))
 fftdata = np.abs(np.fft.fft(data)) / len(data)
@@ -202,6 +223,7 @@ plt.figure(pltnum)
 plt.title("PID controller")
 plt.plot(data)
 
+# Change PID parameters, then run again
 PID_KP = 0x0010
 PID_KI = 0x0005
 PID_KD = 0x0005
@@ -216,41 +238,25 @@ pltnum +=1
 plt.plot(data)
 
 plt.show()
-datapath_word_lut_continuous
-#client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_dist_correction)
+
+# Leave DAC set to arbitrary waveform generator
 client.reg_write(DATAPATH_CONTROL_BASE, datapath_word_lut_continuous)
 
-rev_id = client.reg_read(REV_ID_BASE)
-print ('FPGA load revision: %04X' % rev_id)
-rev_id = client.reg_read(CONTROL_BASE)
-print ('CONTROL_BASE: %04X' % rev_id)
-rev_id = client.reg_read(DATA_READY_BASE)
-print ('DATA_READY_BASE: %04X' % rev_id)
 
-register_list = [
-'REV_ID_BASE          ', 'CONTROL_BASE         ', 'DATA_READY_BASE      ',
-'LED_BASE             ', 'NUM_SAMPLES_BASE     ', 'PID_KP_BASE          ',
-'PID_KI_BASE          ', 'PID_KD_BASE          ', 'PULSE_LOW_BASE       ',
-'PULSE_HIGH_BASE      ', 'PULSE_VAL_BASE       ', 'SYSTEM_CLOCK_BASE    ',
-'DATAPATH_CONTROL_BASE', 'LUT_ADDR_DATA_BASE   ', 'TUNING_WORD_BASE     ',
-'BUFFER_ADDRESS_BASE  ', 'SPI_PORT_BASE        ', 'SPI_RXDATA           ',
-'SPI_TXDATA           ', 'SPI_STATUS           ', 'SPI_CONTROL          ',
-'SPI_SS               ']
+re_read_registers = False
 
-print ('\nReading register block:')
-block = client.reg_read_block(REV_ID_BASE, 22)
-data_reg = block#(ctypes.c_int * 22).from_buffer(bytearray(block))
-for i in range(0, 22):
-    print (register_list[i] + ': %08X'  % data_reg[i])
+if re_read_registers == True:
+    print ('\nReading register block:')
+    block = client.reg_read_block(REV_ID_BASE, 22)
+    data_reg = block
+    for i in range(0, 22):
+        print (register_list[i] + ': %08X'  % data_reg[i])
 
 
-# Okay, now let's try writing the lookup table remotely!
-cDataType = ctypes.c_uint * 65536
-cData     = cDataType()
-
-test_LUT_write = False
-
+# Okay, now let's try writing to the DAC lookup table remotely!
 if(test_LUT_write == True):
+    cDataType = ctypes.c_uint * 65536
+    cData     = cDataType()
     print("Writing downward ramp to LUT!")
     for i in range(0, 65536): # Reverse ramp...
         cData[i] = (i << 16 | (65535 - i))
@@ -288,4 +294,4 @@ if(choice == 'y'):
 #    time.sleep(30)
     client.shutdown()
 
-
+print("Test done! Enter \"client.shutdown()\" to shut down SoCkit board.")
