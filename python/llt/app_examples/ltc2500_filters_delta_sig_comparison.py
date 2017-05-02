7230a063e0549e22edfffe4227918857f18edd29
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2017-05-02 17:49:07 +0000

    Final changes for video
    git-svn-id: svn://subzero.engineering.linear.com:23691/DataConverterEval/linear_lab_tools/trunk@7022 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- python/llt/app_examples/ltc2500_filters_delta_sig_comparison.py
+++ python/llt/app_examples/ltc2500_filters_delta_sig_comparison.py
@@ -66,10 +66,14 @@ FS = 1000000 # Sample rate, for scaling horizontal frequency axis
 
 # Read all filter coefficients into a big 2-d list, calculate corresponding
 # magnitude responses, responses in dB. For each of these, the first index
-# represents the filter type, the second index represents the DF. For example:
+# is the filter type (SINC2, SINC2, SSINC, Flat, etc.),
+# the second index represents the DF. For example:
 # filters[filter type][downsample factor][tap value]
 
+# Reading files and doing these calculations takes a while. Only need to do once
+# if you leave the console open.
 read_files = False
+#read_files = True
 
 if read_files == True:
     filters          = [[[] for j in xrange(len(DF_list))] for i in xrange(len(FT_list))]
@@ -79,9 +83,9 @@ if read_files == True:
     for ft in FT_list:
         dfnum = 0 # Handy numerical index
         for df in DF_list:
-            filename = "../../../common/ltc25xx_filters/" + ft.FT_txt + "_" + df.DF_txt + ".txt"
-            filelength = lltf.linecount(filename)
-            filters[ftnum][dfnum] = np.ndarray(filelength, dtype=float)
+            filename = "../../../common/ltc25xx_filters/" + ft.FT_txt + "_" + df.DF_txt + ".txt" # Construct filename
+            filelength = lltf.linecount(filename) # Find out how long it is
+            filters[ftnum][dfnum] = np.ndarray(filelength, dtype=float) # Add entry to list
             print ("reading " + str(filelength) + " coefficients from file " + filename)
             with open(filename, 'r') as infile: # Read in coefficients from files
                 for i in range(0, filelength):
@@ -92,11 +96,11 @@ if read_files == True:
             filt_resp_mag_db[ftnum][dfnum] = 20*np.log10(abs(filt_resp_mag[ftnum][dfnum])) # Calculate response in dB
             dfnum += 1
         ftnum += 1
-    print ("Done reading in all files, calculating responses!!")
+    print ("Done reading in all coefficient files and calculating responses!!")
 
 # Create filter response of Variable-SINC filter, N=2, to represent the simplest
-# possible filter
-vsinc2 = np.ones(2)
+# possible filter. (This mode is distinct from the other filters, no coefficient file.)
+vsinc2 = np.ones(2) # Yup, that's it... just a couple of 1s!
 vsinc2 /= sum(vsinc2) #Normalize to unity gain
 vsinc_resp_mag = lltf.freqz_by_fft_numpoints(vsinc2, 2 ** 20) # Calculate magnitude response
 vsinc_resp_mag_db = 20*np.log10(abs(vsinc_resp_mag)) # Calculate response in dB
@@ -104,6 +108,8 @@ vsinc_resp_mag_db = 20*np.log10(abs(vsinc_resp_mag)) # Calculate response in dB
 # Plot the impulse responses on the same horizontal axis, with normalized
 # amplitude for a better visual picture...
 plt.figure(1)
+plt.cla()
+plt.ion()
 plt.title('SSINC+Flat filters')
 for df in range(0, len(DF_list)):
     plt.plot(filters[5][df] / max(filters[5][df]))
@@ -117,23 +123,50 @@ haxis = np.linspace(0.0, FS, 2**20) # Horizontal axis
 color_list = ["red","orange"]
 
 # Plot frequency response, linear frequency axis
-lw = 3
+lw = 1.5
 plt.figure(2)
-plt.title("LTC2500-32 filter responses (DF " + DF_info.DF_txt + ")")
+plt.cla()
+plt.ion()
+#plt.cla()
+plt.title("LTC2500-32 filter responses (DF " + DF256.DF_txt + ")")
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Rejection (dB)')
 #plt.axis([0, 16400, -100, 10])
-plt.axis([100, 500000, -100, 10])
-plt.show()
-plt.semilogx(haxis, vsinc_resp_mag_db, linewidth=lw, color="black", zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[0][6], linewidth=lw, color="red", zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[1][6], linewidth=lw, color="orange",  zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[2][6], linewidth=lw, color="green",  zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[3][6], linewidth=lw, color="blue",  zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[4][6], linewidth=lw, color="purple",  zorder=1)
-plt.semilogx(haxis, filt_resp_mag_db[5][6], linewidth=lw, color="black", zorder=1)
+plt.axis([20, 500000, -100, 5])
+# All DF256 filters, for section of video comparing all filter types
+#plt.semilogx(haxis, filt_resp_mag_db[0][6], linewidth=lw, color="red", zorder=1)
+#plt.semilogx(haxis, filt_resp_mag_db[1][6], linewidth=lw, color="orange",  zorder=1)
+#plt.semilogx(haxis, filt_resp_mag_db[2][6], linewidth=lw, color="green",  zorder=1)
+#plt.semilogx(haxis, filt_resp_mag_db[3][6], linewidth=lw, color="blue",  zorder=1)
+#plt.semilogx(haxis, filt_resp_mag_db[4][6], linewidth=lw, color="purple",  zorder=1)
+#plt.semilogx(haxis, filt_resp_mag_db[5][6], linewidth=lw, color="black", zorder=1)
 
+# Selection of filters for section of video discussing versatility, compared
+# with delta sigma
+all_filter_plot = False
+if all_filter_plot == True:
+    plt.semilogx(haxis, vsinc_resp_mag_db, linewidth=lw, color="red", zorder=1) # Simple average of 2 points
+    plt.semilogx(haxis, filt_resp_mag_db[5][6], linewidth=lw, color="black", zorder=1) # Example flat passband filter
+    
+    for dfnum in [0, 2, 4, 6, 8, 10]:# range(0, 11):# [0, 6, 11]:
+        plt.semilogx(haxis[0:2**(24-dfnum)], filt_resp_mag_db[0][dfnum][0:2**(24-dfnum)], linewidth=lw, color="red", zorder=1) # Flat filter, DF4
+        plt.semilogx(haxis, filt_resp_mag_db[1][dfnum], linewidth=lw, color="orange",  zorder=1)
+        plt.semilogx(haxis, filt_resp_mag_db[2][dfnum], linewidth=lw, color="green",  zorder=1)
+        plt.semilogx(haxis, filt_resp_mag_db[3][dfnum], linewidth=lw, color="blue",  zorder=1)
+        plt.semilogx(haxis, filt_resp_mag_db[4][dfnum], linewidth=lw, color="purple",  zorder=1)
+        plt.semilogx(haxis, filt_resp_mag_db[5][dfnum], linewidth=lw, color="black", zorder=1)
 
+# Spread-sinc filters, for section of video describing increasing DF x 2 corresponding
+# to a 3dB improvement in SNR
+filter_bw_plot = True
+if filter_bw_plot == True:
+    ftnum = 4 # SSinc
+    lw = 3
+    plt.axis([500, 100000, -120, 5])
+    plt.semilogx(haxis, filt_resp_mag_db[ftnum][2], linewidth=lw, color="red", zorder=1)
+#    plt.semilogx(haxis, filt_resp_mag_db[ftnum][3], linewidth=lw, color="orange", zorder=1)
+#    plt.semilogx(haxis, filt_resp_mag_db[ftnum][4], linewidth=lw, color="green", zorder=1)
+#    plt.semilogx(haxis, filt_resp_mag_db[ftnum][5], linewidth=lw, color="blue", zorder=1)
 
 
 print "My program took", (time.time() - start_time), " seconds to run"
